0dd12991dad07d8e7212e0a3ddee08d46d83e4ca
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2019-11-11 10:14:59 -0500

    Fixing error in the subsample workflow

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -46,7 +46,6 @@ def check_dependencies_path(config):
                     elif type(value['option']) == list:
                         for a in value['option']:
                             commands.append(a)
-                print(commands)
                 process = subprocess.Popen(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 out, err = process.communicate()
                 if 'stderr' in value['out']:
@@ -56,7 +55,11 @@ def check_dependencies_path(config):
                         print('\nERROR: {0} version: {1} not available'.format(tool, value['version']))
                         print('Tools absolute path: {0}'.format(tool_path))
                         print('Installed version:\n{0}'.format(out.decode('UTF-8')))
+                        print('*************************************************\nCommand:')
+                        print(commands)
+                        print('*************************************************\nOutput:')
                         print(out.decode('UTF-8'))
+                        print('*************************************************')
                         sys.exit(-1)
                 if 'output' in value:
                     if value['output'] not in str(out.decode('UTF-8')).rstrip():
@@ -64,6 +67,8 @@ def check_dependencies_path(config):
                         print('Tools absolute path: {0}'.format(tool_path))
                         print('Expected output:\n{0}'.format(value['output']))
                         print('Real output:\n{0}'.format(out.decode('UTF-8')))
+                        print('*************************************************\nCommand:')
+                        print(commands)
                         print(out.decode('UTF-8'))
                         sys.exit(-1)
 
