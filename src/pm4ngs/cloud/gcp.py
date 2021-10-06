35a4bad859a48458c176aa28641990e20b43814f
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2021-10-06 14:55:29 -0400

    Adding support for editing GCP pipeline

--- src/pm4ngs/cloud/gcp.py
+++ src/pm4ngs/cloud/gcp.py
@@ -1,9 +1,14 @@
 import json
 
 
-def update_pipeline_zone(pipeline_filename, zone_list):
+def update_pipeline(pipeline_filename, zone_list, machineType, local_sdd_size=375,
+                    use_preemptible=True):
     with open(pipeline_filename) as fin:
         pipeline = json.load(fin)
         pipeline['resources']['zones'] = zone_list
+        pipeline['resources']['virtualMachine']['machineType'] = machineType
+        pipeline['resources']['virtualMachine']['preemptible'] = use_preemptible
+        pipeline['resources']['virtualMachine']['disks'][0]['sizeGb'] = local_sdd_size
+    # Writing pipeline back to file
     with open(pipeline_filename, 'w') as fout:
         json.dump(pipeline, fout, indent=4)
